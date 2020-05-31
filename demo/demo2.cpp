71367ccc6c491b706864d676da2e5c6459d6b877
talyorLee <1637153021@qq.com>
2020-05-31 10:15:53 +0800

    just do it

---
+++ demo/demo2.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+#include <cmath>
+#include <vector>
+int main(int argc, char const *argv[])
+{
+    std::string name = "tao kai xin";
+    std::cout << "who love Liwenshan:" << name << std::endl;
+    return 0;
+}
