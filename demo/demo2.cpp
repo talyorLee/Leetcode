7b6aff5f2e76e67fb3538fb1028813735d8b1ab8
talyorLee <1637153021@qq.com>
2020-07-16 23:13:28 +0800

    有点问题

--- demo/demo2.cpp
+++
@@ -1,9 +0,0 @@
-#include "iostream"
-#include <cmath>
-#include <vector>
-int main(int argc, char const *argv[])
-{
-    std::string name = "tao kai xin";
-    std::cout << "who love Liwenshan:" << name << std::endl;
-    return 0;
-}
