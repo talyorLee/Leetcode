b5c7c5a35b57a7c80efbed53e47fde8f51198c33
Li Taylor <taylor.li@sap.com>
2021-09-13 23:21:06 +0800

    230  235:postorder

---
+++ 235.二叉搜索树的最近公共祖先.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=java
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        // p < root && q < root
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        // p > root && q > root
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        // p < root && q > root or p > root && q < root
+        return root;
+    }
+}
+// @lc code=end
