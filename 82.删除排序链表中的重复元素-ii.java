669a156d549f35f77b3a5d4ad8fe7ed78fbebffd
Li Taylor <taylor.li@sap.com>
2021-09-14 12:30:23 +0800

    82 83 linked list

---
+++ 82.删除排序链表中的重复元素-ii.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=82 lang=java
+ *
+ * [82] 删除排序链表中的重复元素 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // 只要涉及头结点的操作，设置虚拟头结点避免对链表第 1 个结点的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode curNode = dummyNode;
+        while (curNode.next != null && curNode.next.next != null) {
+            // 如果接连两个结点的 val 相等，至少要把它们都删掉
+            if (curNode.next.val == curNode.next.next.val) {
+                // 要删除的起点至少应该是当前判断相同的结点的第 2 个
+                ListNode delNode = curNode.next.next;
+                // 如果后面还有相同的结点，delNode 后移一位，即 delNode 应该是指向相同的结点的最后一个
+                while (delNode.next != null && delNode.next.val == delNode.val) {
+                    delNode = delNode.next;
+                }
+                curNode.next = delNode.next;
+                delNode.next = null;
+            } else {
+                curNode = curNode.next;
+            }
+        }
+        return dummyNode.next;
+    }
+}
+// @lc code=end
