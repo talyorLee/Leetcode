31d0e9d225bb7665160e81ad0653ee846465f1e1
Li Taylor <taylor.li@sap.com>
2021-10-27 14:34:10 +0800

    use a special method to validate

---
+++ 331.验证二叉树的前序序列化.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=331 lang=java
+ *
+ * [331] 验证二叉树的前序序列化
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] nodes = preorder.split(",");
+        int diff = 1;
+        for (String node : nodes) {
+            if (--diff < 0)
+                return false;
+            if (!node.equals("#"))
+                diff += 2;
+        }
+        return diff == 0;
+    }
+}
+// @lc code=end
