eca784b173e24dd1f1a7108f9e0c5e602d3b7cdb
Li Taylor <taylor.li@sap.com>
2021-10-30 14:17:37 +0800

    dynamic program

--- 279.完全平方数.java
+++ 279.完全平方数.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode.cn id=279 lang=java
  *
@@ -7,8 +9,16 @@
 // @lc code=start
 class Solution {
     public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; i + j * j <= n; j++) {
+                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp[n];
 
     }
 }
 // @lc code=end
-
---
+++ 746.使用最小花费爬楼梯.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=java
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]);
+            if (i != n)
+                dp[i] += cost[i];
+
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
