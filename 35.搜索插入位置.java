3c9194fa81bf0d8d4798591977ebd1fc2b4a09bd
Li Taylor <taylor.li@sap.com>
2021-11-13 21:39:54 +0800

    binary search

---
+++ 35.搜索插入位置.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+}
+// @lc code=end
