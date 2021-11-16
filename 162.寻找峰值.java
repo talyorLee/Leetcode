f179520b683f2a4bb0a15c0e661b28193ef47e85
Li Taylor <taylor.li@sap.com>
2021-11-16 12:44:41 +0800

    array

---
+++ 162.寻找峰值.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=java
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                if (nums[i] > nums[i + 1]) {
+                    return i;
+                }
+            } else if (i == nums.length - 1) {
+                if (nums[i] > nums[i - 1]) {
+                    return i;
+                }
+            } else {
+                if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                    return i;
+                }
+                if (nums[i] >= nums[i + 1]) {
+                    i++;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
+// @lc code=end
