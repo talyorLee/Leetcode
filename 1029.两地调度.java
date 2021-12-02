e41322465c5c5d8f92d3f384a4523ac109424366
Li Taylor <taylor.li@sap.com>
2021-12-02 15:01:44 +0800

    greedy algo

--- 1029.两地调度.java
+++ 1029.两地调度.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /*
  * @lc app=leetcode.cn id=1029 lang=java
  *
@@ -7,8 +10,18 @@
 // @lc code=start
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-
+        Arrays.sort(costs, Comparator.comparingInt(a -> (a[0] - a[1])));
+        // 前一半取去 A 市，后一半取去 B 市，前一半是去 A 市最合适
+        // 后一半市去 B 市最合适
+        int sum = 0;
+        for (int i = 0; i < costs.length; ++i) {
+            if (i < costs.length / 2) {
+                sum += costs[i][0];
+            } else {
+                sum += costs[i][1];
+            }
+        }
+        return sum;
     }
 }
 // @lc code=end
-
